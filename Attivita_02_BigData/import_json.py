2c28a29c262ccc3d8a78a61602fa95510cbe990f
ElenaCurti <104670814+ElenaCurti@users.noreply.github.com>
2022-11-23 09:07:02 +0000

    attivita 2: messo for in basso

--- Attivita_02_BigData/import_json.py
+++ Attivita_02_BigData/import_json.py
@@ -13,10 +13,12 @@ with open(nome_file_json) as data_file:
     dati_json = json.load(data_file)
 lista_json = list(dati_json)
 
-# (Questo for va messo solo in fase di debug)
-for i in range(len(lista_json)):
-    r.delete(nome_database+":"+str(i)) 
-
 # ... e lo salvo sul database
 for i in range(len(lista_json)):
     r.json().set(nome_database+":"+str(i), Path.root_path(), lista_json[i])
+
+
+
+# (Questo for va messo solo in fase di debug)
+for i in range(len(lista_json)):
+    r.delete(nome_database+":"+str(i)) 
