981e754b93b425da558ff4d84433410e9ecc5ecc
ElenaCurti <104670814+ElenaCurti@users.noreply.github.com>
2022-11-23 09:06:22 +0000

    attivita 2: cambiato import

--- Attivita_02_BigData/import_json.py
+++ Attivita_02_BigData/import_json.py
@@ -5,28 +5,18 @@ from redis.commands.json.path import Path
 nome_file_json = "nobelPrizes.json"
 nome_database = 'premi_nobel'
 
-
 # Apro la connessione col server
 r = redis.Redis(db=0)
 
 # Recupero il contenuto del file...
 with open(nome_file_json) as data_file: 
     dati_json = json.load(data_file)
-
 lista_json = list(dati_json)
 
-# Questo for va messo solo in fase di debug
+# (Questo for va messo solo in fase di debug)
 for i in range(len(lista_json)):
-    lista_json[i]['idNobel'] = i
+    r.delete(nome_database+":"+str(i)) 
 
-r.json().set(nome_database, Path.root_path(), dati_json)
-
-# # ... e lo salvo sul database
-# lista_json = list(dati_json)
-
-# # Questo for va messo solo in fase di debug
-# for i in range(len(lista_json)):
-#     r.delete(nome_database+":"+str(i)) 
-
-# for i in range(len(lista_json)):
-#     r.json().set(nome_database+":"+str(i), Path.root_path(), lista_json[i])
+# ... e lo salvo sul database
+for i in range(len(lista_json)):
+    r.json().set(nome_database+":"+str(i), Path.root_path(), lista_json[i])
