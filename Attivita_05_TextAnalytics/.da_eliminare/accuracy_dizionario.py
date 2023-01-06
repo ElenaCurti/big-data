ceccd9edf2b564a8c43269e6b7a700907661c751
ElenaCurti <elena.curti.00@gmail.com>
2023-01-06 13:01:24 +0100

    attivita 5: applicazione quasi ok

---
+++ Attivita_05_TextAnalytics/.da_eliminare/accuracy_dizionario.py
@@ -0,0 +1,32 @@
+accuracy1 = 0.8604			#0.8686
+accuracy2 = 0.8548			#0.87
+accuracy3 = 0.412				#0.41
+accuracy4 = 0.7894			#0.7896
+accuracy5 = 0.5962			#0.5864
+accuracy6 = 0.8752			#0.875
+
+#(con word frequency)
+accuracy7 = 0.8706				
+accuracy8 = 0.8584			
+accuracy9 = 0.8174 		# con word occurrency: 0.83	
+			
+#(con dimensione di test set = dimensione di train set:)
+accuracy10 = 0.8774			
+accuracy11 = 0.8742			
+accuracy12 = 0.8882				
+accuracy13 = 0.7902		
+
+dizionario_plot = {}
+dizionario_plot["1.Originale"] = accuracy1
+dizionario_plot["2.ChrisPott"] = accuracy2
+dizionario_plot["3.Cinque classi"] = accuracy3
+dizionario_plot["4.Summary"] = accuracy4
+dizionario_plot["5.Solo aggettivi"] = accuracy5
+dizionario_plot["6.Frequency word"] = accuracy6
+dizionario_plot["7.WordNetLemmatizer"] = accuracy7
+dizionario_plot["8.Spacy"] = accuracy8
+dizionario_plot["9.lemmatize,opinion_lexicon"] = accuracy9
+dizionario_plot["10.SVM"] = accuracy10
+dizionario_plot["11.Hyperlane"] = accuracy11
+dizionario_plot["12.Grid-search"] = accuracy12
+dizionario_plot["13.Xgboost"] = accuracy13
